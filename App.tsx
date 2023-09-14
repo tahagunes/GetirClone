d47dd557bd5ebdf107c2b64fc0fea00df1a3a520
Osman Taha Güneş <ttahaa70@gmail.com>
2023-09-14 19:40:29 +0300

    #2

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import HomeScreen from "./src/screens/HomeScreen"
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomeScreen/>
     </View>
   );
 }
@@ -14,7 +13,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-});
+}); 
---
+++ src/screens/HomeScreen/styles.ts
@@ -0,0 +1,42 @@
+import { StyleSheet , Dimensions} from "react-native";
+
+const {height} = Dimensions.get('window');
+const styles = StyleSheet.create({
+headerMain:{
+    marginTop:35,
+    height: height * 0.064,
+    backgroundColor: '#F7D102',
+},
+headerOne:{
+    backgroundColor: 'white',
+    width: '80%',
+    height: height * 0.064,
+    flexDirection: 'row',
+    alignItems:'center',
+    paddingHorizontal:'3%',
+    borderTopRightRadius:25,
+    borderBottomRightRadius:25,
+},
+headerOneView:{
+    flexDirection:'row',
+    alignItems:'center',
+    marginLeft:8,
+    paddingLeft:8,
+    borderLeftColor:'#F3F2FD',
+    borderLeftWidth:2,
+},
+image:{
+width:30,
+height:30
+},
+headerTwo:{
+    height: height * 0.064,
+    width:'20%',
+//backgroundColor:'red',
+flexDirection:'column',
+justifyContent:'center',
+alignItems:'center',
+paddingLeft:10,
+},
+})
+export default styles;
